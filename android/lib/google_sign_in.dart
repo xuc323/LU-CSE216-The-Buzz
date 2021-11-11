34b15dd18cb69391a125431dfc4d62311228d52f
sts223 <sts223@lehigh.edu>
2021-11-10 20:19:46 -0500

    fixing error

--- android/lib/google_sign_in.dart
+++ android/lib/google_sign_in.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:http/http.dart'as http;
+import 'package:http/http.dart' as http;
 
 class GoogleSignInProvider extends ChangeNotifier {
   final googleSignIn = GoogleSignIn();
@@ -23,15 +25,16 @@ class GoogleSignInProvider extends ChangeNotifier {
 
     notifyListeners();
   }
-    Future<http.Response> TokenBackend(String title) {
-  return http.post(
-    Uri.parse('https://the-buzz-group25.herokuapp.com/messages'),
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: jsonEncode(<String, String>{
-      'title': title,
-    }),
-  );
-}
+
+  Future<http.Response> TokenBackend(String title) {
+    return http.post(
+      Uri.parse('https://the-buzz-group25.herokuapp.com/messages'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'title': title,
+      }),
+    );
+  }
 }
